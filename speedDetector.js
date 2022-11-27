c00afd201edafc4a3f39844703bea12986ce975c
Glen <glen.kimani@student.moringaschool.com>
2022-11-28 01:35:27 +0300

    two completed

---
+++ speedDetector.js
@@ -0,0 +1,30 @@
+//Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70, it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points.
+
+//For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
+
+ let speed = prompt("Input speed of a car(km/hr): ", "");
+if (speed !== null && speed >= 0)
+{
+    let driverLicenseInfo;
+    if(speed < 70)
+    {
+        driverLicenseInfo = "Ok"
+    }
+    else
+    {
+        let demeritPoint = Math.ceil((speed - 70)/5);
+        if (demeritPoint < 12)
+        {
+            driverLicenseInfo = `Demerit Points: ${demeritPoint}`
+        }
+        else
+        {
+            driverLicenseInfo = `Demerit Points: ${demeritPoint}, LICENSE SUSPENDED!`
+        }
+    }
+    alert(driverLicenseInfo)
+}
+else
+{
+    alert('Wrong Input')
+} 
---
+++ studenGradeGeneratory.js
@@ -0,0 +1,33 @@
+//Write a program that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade as follows: 
+
+//A > 79, B > 60 to 79, C > 59 to 49, D > 40 to 49, E > less 40.
+
+ let marks = prompt("Present your marks here", "");
+if(marks !== null && marks >= 0 && marks <= 100)
+{
+    function grades(marks)
+    {
+        let grade;
+        if (marks > 79)
+        {
+            grade = "A"
+        }
+        else if (marks > 60)
+        {
+            grade = "B"
+        }
+        else if (marks > 49)
+        {
+            grade = "C"
+        }
+        else if (marks > 40)
+        {
+            grade = "D"
+        }
+        else{
+            grading = "E"
+        }
+        return grade
+    }
+    alert(`Your grade is; ${grading(marks)}`);
+}
